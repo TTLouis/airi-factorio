ce323a5e9d13644dffd0e4a724ae894574cf9b70
TTLouis <Louisefu135@gmail.com>
2026-09-13 12:29:52 -0700

    refactor(agent): call actor-based Autorio tools

--- packages/agent/src/llm/tools.ts
+++ packages/agent/src/llm/tools.ts
@@ -14,24 +14,24 @@ interface ToolFunction {
 export const tools: ToolFunction[] = [
   {
     name: 'getInventoryItems',
-    description: 'Get the items in the player\'s inventory',
+    description: 'Get the items in AIRI\'s controlled actor inventory',
     schema: z.object({}),
     fn: async () => {
-      const response = await v2FactorioConsoleCommandRawPost({ body: { input: '/c remote.call("autorio_tools", "get_inventory_items", 1)' } })
+      const response = await v2FactorioConsoleCommandRawPost({ body: { input: '/c remote.call("autorio_tools", "get_inventory_items")' } })
       logger.withFields({ response: response.data.output }).debug('Inventory items')
       return response.data.output
     },
   },
   {
     name: 'getRecipe',
-    description: 'Get the recipe for a given item',
+    description: 'Get the recipe for a given item for AIRI\'s controlled actor',
     schema: z.object({
       item: z.string().describe('The item to get the recipe for'),
     }),
     fn: async ({ parameters }) => {
       logger.withFields(parameters).debug('Try to get recipe for item')
 
-      const response = await v2FactorioConsoleCommandRawPost({ body: { input: `/c remote.call("autorio_tools", "get_recipe", "${parameters.item}", 1)` } })
+      const response = await v2FactorioConsoleCommandRawPost({ body: { input: `/c remote.call("autorio_tools", "get_recipe", "${parameters.item}")` } })
       logger.withFields({ response: response.data.output }).debug('Recipe')
       return response.data.output
     },
