7e52924fb7197f834db7f02291cc164d7382c171
TTLouis <Louisefu135@gmail.com>
2026-09-17 18:37:06 -0700

    feat(skills): expose and seed basic patterns

--- packages/autorio/src/control.ts
+++ packages/autorio/src/control.ts
@@ -30,6 +30,7 @@ import { create_production_planning_remote_interface } from './production_planni
 import { create_prototype_knowledge_remote_interface } from './prototype_knowledge'
 import { new_recipe_configuration_runtime } from './recipe_configuration'
 import { new_research_controller } from './research'
+import { create_skill_remote_interface, ensure_basic_skill_definitions } from './skills'
 import { with_research_trigger } from './research_trigger'
 import { new_task_manager } from './task_manager'
 import { create_task_board_ui_remote_interface, set_task_board_world_task_provider } from './task_board_ui'
@@ -44,6 +45,7 @@ create_knowledge_remote_interface(get_controlled_actor)
 create_prototype_knowledge_remote_interface(get_controlled_actor)
 create_production_planning_remote_interface(get_controlled_actor)
 create_task_board_ui_remote_interface()
+create_skill_remote_interface()
 
 let setup_complete = false
 
@@ -457,7 +459,9 @@ script.on_event(defines.events.on_player_mined_entity, (event: OnPlayerMinedEnti
 })
 
 function setup() {
+  const seeded = ensure_basic_skill_definitions()
   setup_complete = true
+  if (seeded.added > 0) log(`[AUTORIO] Seeded ${seeded.added}/${seeded.total} curated basic skill patterns`)
   log('[AUTORIO] Setup complete')
 }
 
